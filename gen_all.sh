b8cbe913f8133d6811b5e06defbc8041f497f6ef
tnelsond <tnelsond@gmail.com>
2025-08-17 22:14:23 +0700

    fixed missing color

--- gen_all.sh
+++ gen_all.sh
@@ -1,4 +1,4 @@
-bash gen.sh lao #770000 #110077
-bash gen.sh khmer #770044 #DDDDDD
-bash gen.sh chitonga #666600 #003377
-bash gen.sh lozi #330099 #995500
+bash gen.sh lao "#770000" "#110077"
+bash gen.sh khmer "#770044" "#DDDDDD"
+bash gen.sh chitonga "#666600" "#003377"
+bash gen.sh lozi "#330099" "#995500"
