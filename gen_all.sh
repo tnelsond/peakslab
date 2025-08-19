59d6379d9cf1012e465b61f382690d0c3568dbb2
tnelsond <tnelsond@gmail.com>
2025-08-19 08:36:48 +0700

    fixed interface

--- gen_all.sh
+++ gen_all.sh
@@ -1,4 +1,4 @@
-bash gen.sh lao "#770000" "#110077"
+bash gen.sh lao "#770000" "#DD4466"
 bash gen.sh khmer "#770044" "#DDDDDD"
-bash gen.sh chitonga "#666600" "#003377"
-bash gen.sh lozi "#330099" "#995500"
+bash gen.sh chitonga "#666600" "#5588ff"
+bash gen.sh lozi "#3300BB" "#995500"
