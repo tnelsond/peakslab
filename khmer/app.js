36632a9d8f87d0eaacc143322f773b6ae908b6db
tnelsond <tnelsond@gmail.com>
2026-01-07 22:21:00 +0700

    fixed sw path and scope

--- khmer/app.js
+++ khmer/app.js
@@ -596,7 +596,7 @@ initWasm().then(() => {
 const registerServiceWorker = async () => {
 	if ("serviceWorker" in navigator) {
 		try {
-			const registration = await navigator.serviceWorker.register("dictsw.js", {scope: '/lao/'});
+			const registration = await navigator.serviceWorker.register("sw.js", {scope: '/khmer/'});
 			if (registration.installing) {
 				console.log("Service worker installing");
 			} else if (registration.waiting) {
