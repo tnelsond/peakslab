0dcf9c5174909c304ce443fe6130d11b2974da62
tnelsond <tnelsond@gmail.com>
2026-03-11 14:57:26 +0700

    Converted lozi dictionary over. Removed some old files.

---
+++ lozi/config.js
@@ -0,0 +1,12 @@
+const tablayout = [
+	{name: "Dict", dicts: [
+		["lozi/db/lozi.peak.zst", "lozi", 8*1024, `From some silozi dictionary. Needs to be cleaned up.`],
+	]}
+];
+
+const scope = '/lozi/';
+const lang = [
+	{name: "Lozi", val: 'loz-ZM'},
+	{name: "English", val: 'en-US'}
+];
+const appname = 'lozi';
