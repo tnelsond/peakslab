e6a013877fcb3f6e7ace162b1e2915248742c4ab
tnelsond <tnelsond@gmail.com>
2026-03-12 21:26:55 +0700

    Added khmer proverbs

--- khmer/config.js
+++ khmer/config.js
@@ -7,6 +7,7 @@ const tablayout = [
 		["khmer/db/ant.peak.zst", "ANT", 32*1024, `<a href="https://antkh.com/">antkh.com</a>`],
 		["khmer/db/nath2022_8.peak.zst", "Km→Km", 32*1024, `2022 version of the <a href="https://play.google.com/store/apps/details?id=com.optimiskh.chuonnathdictionary&hl=en-US">Chuon Nath Digital Dictionary</a>.`],
 		["khmer/db/seacount.peak.zst", "Count", 1*1024, `via <a href="https://sealang.net/khmer/dictionary.htm">Sealang</a>.`],
+		["khmer/db/choukprov.peak.zst", "Proverbs", 8*1024, `Proverbs from <a href="https://choukhmer.wordpress.com/literature/descriptive-proverbs-kar/">Chouk Khmer</a>.`],
 	]},
 	{name: "♫", dicts: [
 		["khmer/db/hymns7.peak.zst", "Hymn", 32*1024, `<a href="https://www.khmerworship.com/">Khmer Worship</a>`],
--- utils/usfmtotsv.pl
+++ utils/usfmtotsv.pl
@@ -1,2 +1,35 @@
+#/usr/bin/perl
 use strict;
+use warnings;
+use utf8;
 
+binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+use open qw/ :std :encoding(UTF-8) /;
+
+my %abr = qw(JHN JOH
+						JOL JOE
+						PHP PHI
+						EZK EZE
+						SNG SOL
+						1JN 1JO
+						2JN 2JO
+						3JN 3JO
+						JAS JAM
+						MRK MAR);
+my $bookabr = "NULL";
+my $book = "NULL";
+my $chapter = "NULL";
+while(<>){
+	if(/^\\id (.*)/){
+		$bookabr = $abr{$1};
+		if(!$bookabr){$bookabr = $1}
+	}elsif(/^\\h (.*)/){
+		$book = $1;
+	}elsif(/^\\c (.*)/){
+		$chapter = $1;
+	}elsif(/^\\v ([0-9]+) (.*)/){
+		print "$bookabr $chapter:$1\t<p-n>\@$book $chapter:$1</p-n>\t$2\n"
+	}
+}
