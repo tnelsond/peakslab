f83279ae5b00579bb6396dcc6497f6d3b51df409
tnelsond <tnelsond@gmail.com>
2026-04-11 10:18:52 +0700

    Added german.

---
+++ german/config.js
@@ -0,0 +1,13 @@
+const tablayout = [
+	{name: "Bible", dicts: [
+		["german/db/deuSchBible.peak.zst", "deBible", 4*1024, `Die Schlachter Bibel 1951 from <a href="https://ebible.org/details.php?id=deu1951">ebible</a>`],
+		["english/db/engULB.peak.zst", "En_ULB", 8*1024, `English Unlocked Literal Bible from <a href="https://door43.org">Door43</a>, Wycliffe Associates`],
+	]}
+];
+
+const scope = '/german/';
+const lang = [
+	{name: "German", val: 'de_GE'},
+	{name: "English", val: 'en_US'}
+];
+const appname = 'de';
